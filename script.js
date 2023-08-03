1ca26146263301e681be344e714cd6d02dc73755
Hoang Quoc <quochoang803@gmail.com>
2023-08-03 16:00:50 +0700

    new

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-import data from '/data.js';
+const data = {
+    A: ["A1.png","A2.png","A3.png"],
+    B: ["B1.png","B2.png","B3.png"],
+    I: ["I1.png","I2.png","I3.png","I4.png"],
+    R: ["R1.png","R2.png","R3.png","R4.png"],
+    T: ["T1.png","T2.png","T3.png","T4.png"],
+    H: ["H1.png","H2.png","H3.png"],
+    D: ["D1.png","D2.png","D3.png","D4.png"],
+    Y: ["Y1.png","Y2.png","Y3.png","Y4.png"],
+    P: ["P1.png","P2.png","P3.png"]
+};
 function animation(){
     let happy = document.getElementById('happy');
     let birthday = document.getElementById('birthday');
